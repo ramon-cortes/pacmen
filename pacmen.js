59a603bc5147b53ba29b67b1ed5305235463c7c8
ramon-cortes <110551645+ramon-cortes@users.noreply.github.com>
2022-10-07 11:16:19 -0500

    Update pacmen.js

--- pacmen.js
+++ pacmen.js
@@ -1,7 +1,7 @@
 var pos = 0;
 const pacArray = [
-    ['/images/PacMan1.png', '/images/PacMan2.png'],
-    ['/images/PacMan3.png', '/images/PacMan4.png']
+    ['images/PacMan1.png', 'images/PacMan2.png'],
+    ['images/PacMan3.png', 'images/PacMan4.png']
 ];
 var direction = 0;
 const pacMen = []; // This array holds all the pacmen
